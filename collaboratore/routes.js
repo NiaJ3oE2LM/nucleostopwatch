849d0e0c5bf5cb2b23d3c0034458e4ba68005d3f
Michele <schio.michele@gmail.com>
2017-02-14 13:21:22 +0100

    interfaccia utente con tabelle
    meglio div? tocca usare angular

--- collaboratore/routes.js
+++ collaboratore/routes.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();;
 
 // select from req.bosy.table
-router.get('/', function(req, res, next) {
+router.get('/gara', function(req, res, next) {
   res.render("gara");
+})
+.get('/registrazione', function(req, res, next) {
+  res.render("registrazione");
 });
 
 module.exports = router;
--- tabellone/routes.js
+++ tabellone/routes.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();;
 
 // select from req.bosy.table
-router.get('/', function(req, res, next) {
+router.get('/programma', function(req, res, next) {
   res.render('programma');
+})
+.get('/gara', function(req, res, next) {
+  res.render('gara');
 });
 
 module.exports = router;
